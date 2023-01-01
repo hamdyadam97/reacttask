0e6934bca21a468964dbdec2fae8a430f32e9eff
hamdyadam97 <hamdyadam97@gmail.com>
2023-01-01 22:02:25 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Formm from './Component/Form';
+import Thanks from './Component/Thanks';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+     
+      <Routes>
+
+        <Route exact path={"/"} element={<Formm/>} />
+        <Route exact path={"/thanks"} element={<Thanks/>} />
+
+
+      </Routes>
+
+    </BrowserRouter>
+
   );
 }
 
